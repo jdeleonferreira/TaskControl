107ae8116adff264f2ccc66ca9b3cbdfd4b625a9
jdeleon <jdeleonferreira@gmail.com>
2022-10-02 11:32:03 -0500

    Added Swagger config, entities and Task controller.

---
+++ TaskControl/Entities/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskControl.Entities;
+
+namespace TaskControl.Entities
+{
+    public partial class Task
+    {
+        public Task()
+        {
+            TaskMembers = new HashSet<TaskMember>();
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int StatusId { get; set; }
+        public int WorkspaceId { get; set; }
+
+        public virtual Status Status { get; set; } = null!;
+        public virtual Workspace Workspace { get; set; } = null!;
+        public virtual ICollection<TaskMember> TaskMembers { get; set; }
+    }
+
+}
---
+++ TaskControl/Entities/TaskRecord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskControl.Entities
+{
+    public partial class TaskRecord
+    {
+        public Guid TaskId { get; set; }
+        public string Action { get; set; } = null!;
+        public DateTime CreatedDate { get; set; }
+    }
+}
